4a257e4fc4d2c274a05491679c1fa4f1a2111fe8
Thilina Rajakaruna <thilinar@jrcsoftware.com>
2022-03-13 21:40:34 +0530

    structural changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { CreateOrganizationComponent } from './components/organization/create-organization/create-organization.component';
 import { OrganizationsListComponent } from './components/organization/organizations-list/organizations-list.component';
-
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CreateDepartmentComponent } from './pages/departments/create-department/create-department.component';
-import { DepartmentsListComponent } from './pages/departments/departments-list/departments-list.component';
-import { ViewDepartmentComponent } from './pages/departments/view-department/view-department.component';
-import { EmployeeListComponent } from './pages/employee/employee-list/employee-list.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { CreateTeamComponent } from './pages/teams/create-team/create-team.component';
-import { TeamListComponent } from './pages/teams/team-list/team-list.component';
-import { ViewTeamComponent } from './pages/teams/view-team/view-team.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-
+import { AdminLayoutComponent } from './components/layout/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './components/layout/auth-layout/auth-layout.component';
+import { CreateDepartmentComponent } from './components/departments/create-department/create-department.component';
+import { DepartmentsListComponent } from './components/departments/departments-list/departments-list.component';
+import { ViewDepartmentComponent } from './components/departments/view-department/view-department.component';
+import { TeamListComponent } from './components/teams/team-list/team-list.component';
+import { CreateTeamComponent } from './components/teams/create-team/create-team.component';
+import { ViewTeamComponent } from './components/teams/view-team/view-team.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'organization/list', pathMatch: 'full' },
   { path: 'organization/list', component: OrganizationsListComponent },
   { path: 'organization/create', component: CreateOrganizationComponent },
-   //admin routing
-  {path: 'admin', component: AdminLayoutComponent,
+  //admin routing
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
     children: [
-      { path: 'dashboard',            component: DashboardComponent},
-      { path: 'departments-list',     component: DepartmentsListComponent},
-      { path: 'create-department',    component: CreateDepartmentComponent },
-      { path: 'view-department',      component: ViewDepartmentComponent },
-      { path: 'team-list',            component: TeamListComponent},
-      { path: 'create-team',          component: CreateTeamComponent},
-      { path: 'view-team',            component: ViewTeamComponent },
-      { path: 'user-profile',         component: UserProfileComponent},
-      //{ path: 'create-PA',   component: CreatePAsheetComponent},
-      { path: 'employee-list',         component: EmployeeListComponent},
-      { path: "", redirectTo: "dashboard", pathMatch: "full" },
-    ]
+      { path: 'departments-list', component: DepartmentsListComponent },
+      { path: 'create-department', component: CreateDepartmentComponent },
+      { path: 'view-department', component: ViewDepartmentComponent },
+      { path: 'team-list', component: TeamListComponent },
+      { path: 'create-team', component: CreateTeamComponent },
+      { path: 'view-team', component: ViewTeamComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
   },
   //auth routing
-  {path: 'auth', component: AuthLayoutComponent,
-    children: [
-      { path: 'login',    component: LoginComponent},
-      { path: 'signup',   component: SignupComponent},
-      { path: "", redirectTo: "signup", pathMatch: "full" },
-    ]
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [{ path: '', redirectTo: 'signup', pathMatch: 'full' }],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SideNavComponent } from './components/layout/side-nav/side-nav.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 import { OrganizationsListComponent } from './components/organization/organizations-list/organizations-list.component';
 import { CreateOrganizationComponent } from './components/organization/create-organization/create-organization.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { LoginComponent } from './pages/login/login.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { CreateDepartmentComponent } from './pages/departments/create-department/create-department.component';
-import { DepartmentsListComponent } from './pages/departments/departments-list/departments-list.component';
-import { ViewDepartmentComponent } from './pages/departments/view-department/view-department.component';
-import { ViewTeamComponent } from './pages/teams/view-team/view-team.component';
-import { TeamListComponent } from './pages/teams/team-list/team-list.component';
-import { CreateTeamComponent } from './pages/teams/create-team/create-team.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
-import { EmployeeListComponent } from './pages/employee/employee-list/employee-list.component';
+import { AdminLayoutComponent } from './components/layout/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './components/layout/auth-layout/auth-layout.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { CreateDepartmentComponent } from './components/departments/create-department/create-department.component';
+import { DepartmentsListComponent } from './components/departments/departments-list/departments-list.component';
+import { ViewDepartmentComponent } from './components/departments/view-department/view-department.component';
+import { ViewTeamComponent } from './components/teams/view-team/view-team.component';
+import { CreateTeamComponent } from './components/teams/create-team/create-team.component';
+import { TeamListComponent } from './components/teams/team-list/team-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent,
     OrganizationsListComponent,
     CreateOrganizationComponent,
     AdminLayoutComponent,
     AuthLayoutComponent,
-    DashboardComponent,
-    SignupComponent,
-    LoginComponent,
     FooterComponent,
     SidebarComponent,
     NavbarComponent,
@@ -47,8 +37,6 @@ import { EmployeeListComponent } from './pages/employee/employee-list/employee-l
     ViewTeamComponent,
     TeamListComponent,
     CreateTeamComponent,
-    UserProfileComponent,
-    EmployeeListComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
